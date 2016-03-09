6aa5318dd81b87e3c526c5864ed152a8bbbdeeba
byklo <lo.bryan8@gmail.com>
2016-03-09 14:33:19 -0500

    updated

--- blo/testy.py
+++ blo/testy.py
@@ -121,49 +121,140 @@ mem('enter')
 
 #### idx?
 # yes, x[idx] allocates new memory for a copy, don't do it man
-x = np.arange(2500).reshape(50, 50)
-mem('made x')
-print(x)
+# x = np.arange(2500).reshape(50, 50)
+# mem('made x')
+# print(x)
 
-indices = sorted(set(np.random.randint(0, 50, 6)))
-print(indices)
-idx = np.ix_(indices, indices)
-# idx = np.ix_(range(20, 15000), range(20, 15000))
-mem('made idx')
+# indices = sorted(set(np.random.randint(0, 50, 6)))
+# print(indices)
+# idx = np.ix_(indices, indices)
+# # idx = np.ix_(range(20, 15000), range(20, 15000))
+# mem('made idx')
 
-# y = x[idx]
-# mem('x[idx]')
+# # y = x[idx]
+# # mem('x[idx]')
 
-z = x[idx]
-print(z)
-# z = x[20:15000, 20:15000]
-mem('z')
+# z = x[idx]
+# print(z)
+# # z = x[20:15000, 20:15000]
+# mem('z')
 
-def inMemFilterFps(x, mask):
-    if x.shape[0] == x.shape[1]:
-        for i, m1 in enumerate(mask):
-            for j, m2 in enumerate(mask):
-                x[i, j] = x[m1, m2]
-        # zero out "top right quadrant" of matrix after masking
-        for i in xrange(len(mask)):
-            for j in xrange(len(mask), x.shape[0]):
-                x[i, j] = 0
-        # zero out "bottom quadrants" of matrix after masking
-        for i in xrange(len(mask), x.shape[0]):
-            for j in xrange(x.shape[0]):
-                x[i, j] = 0
-        # return a view, does not allocate new memory
-        return x[:len(mask), :len(mask)]
-    else:
-        return x[mask, :]
+# def inMemFilterFps(x, mask):
+#     if x.shape[0] == x.shape[1]:
+#         for i, m1 in enumerate(mask):
+#             for j, m2 in enumerate(mask):
+#                 x[i, j] = x[m1, m2]
+#         # zero out "top right quadrant" of matrix after masking
+#         for i in xrange(len(mask)):
+#             for j in xrange(len(mask), x.shape[0]):
+#                 x[i, j] = 0
+#         # zero out "bottom quadrants" of matrix after masking
+#         for i in xrange(len(mask), x.shape[0]):
+#             for j in xrange(x.shape[0]):
+#                 x[i, j] = 0
+#         # return a view, does not allocate new memory
+#         return x[:len(mask), :len(mask)]
+#     else:
+#         return x[mask, :]
 
-zz = inMemFilterFps(x, indices)
-print(zz)
+# zz = inMemFilterFps(x, indices)
+# print(zz)
 
-print(x.shape)
-print(zz.shape)
+# print(x.shape)
+# print(zz.shape)
 
-print(x[:9, :9])
+# print(x[:9, :9])
 
-mem('end')
 #### idx?
+
+
+#### row vs element
+
+# x = np.arange(1000000).reshape(1000, 1000);
+# N = float(x.shape[0]**2)
+
+# mean = 0
+
+# # for i in xrange(1000):
+# #     for j in xrange(1000):
+# #         mean += x[i, j] / N
+
+# for i in xrange(1000):
+#     mean += np.sum(x[i, ] / N)
+
+# print(mean)
+
+
+#### row vs element 2
+
+vec1 = np.loadtxt('./vec1.txt.gz', ndmin=2)
+vec2 = np.loadtxt('./vec2.txt.gz', ndmin=2)
+
+# np.savetxt('vec1.txt.gz', vec1)
+# np.savetxt('vec2.txt.gz', vec2)
+
+m1 = 0.0
+m2 = 0.0
+N = vec1.shape[0]
+M = float(N**2)
+
+print M
+
+# for i in xrange(N):
+#     for j in xrange(N):
+#         # cast point if using int8 matrices
+#         m1 += vec1[i, j] / M
+#         m2 += vec2[i, j] / M
+# print m1
+# print m2
+# print m1 * M
+# print m2 * M
+
+for i in xrange(N):
+    m1 += np.add.reduce(vec1[i, :N])
+    m2 += np.add.reduce(vec2[i, :N])
+m1 /= M
+m2 /= M
+print m1
+print m2
+
+# for i in xrange(N):
+#     for j in xrange(N):
+#         # cast point if using int8 matrices
+#         m1 += vec1[i, j]
+#         m2 += vec2[i, j]
+# print m1
+# print m2
+# m1 /= M
+# m2 /= M
+
+# print("m1 %.16f | m2 %.16f" % (m1, m2))
+
+# print(vec1[:10, :10])
+# print((vec1[3, :4] + 2)**2)
+print("OG")
+
+s1_1 = 0
+s2_1 = 0
+for i in xrange(N):
+    for j in xrange(N):
+        s1_1 += ((vec1[i, j] - m1)**2) / (M - 1)
+        s2_1 += ((vec2[i, j] - m2)**2) / (M - 1)
+print("s1_1 %.20f | s2_1 %.20f" % (s1_1 * (M - 1), s2_1 * (M - 1)))
+print("s1_1 %.20f | s2_1 %.20f" % (s1_1, s2_1))
+
+print("MINE")
+
+s1_2 = 0
+s2_2 = 0
+for i in xrange(N):
+    s1_2 += np.add.reduce((vec1[i, ] - m1)**2)
+    s2_2 += np.add.reduce((vec2[i, ] - m2)**2)
+print("s1_2 %.20f | s2_2 %.20f" % (s1_2, s2_2))
+s1_2 /= (M - 1)
+s2_2 /= (M - 1)
+print("s1_2 %.20f | s2_2 %.20f" % (s1_2, s2_2))
+
+
+####
+mem('end')
